104b639bd36b02ca913489f2a5dec74977ca7eae
wizjany <wizjany@gmail.com>
2025-09-02 16:00:36 -0400

    Try not to make bad schema/binding commits.

--- scripts/generate-bindings/get-schema.sh
+++ scripts/generate-bindings/get-schema.sh
@@ -10,5 +10,5 @@ output_dir=${DATA_DIR:-workspace/bindings}
 # Create output directory if it doesn't exist
 mkdir -p "$output_dir"
 
-curl "https://${hostname}/v1/database/bitcraft-global/schema?version=9" -o "${output_dir}/global_schema.json"
-curl "https://${hostname}/v1/database/bitcraft-2/schema?version=9" -o "${output_dir}/region_schema.json"
+curl --fail-with-body "https://${hostname}/v1/database/bitcraft-global/schema?version=9" -o "${output_dir}/global_schema.json"
+curl --fail-with-body "https://${hostname}/v1/database/bitcraft-2/schema?version=9" -o "${output_dir}/region_schema.json"
