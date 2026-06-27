d98fba829a32a35db12ae6b8dfdc9783df5f0873
wizjany <wizjany@gmail.com>
2026-06-27 09:52:21 -0400

    Extra asset conversions for UI elements.

--- scripts/assets/map_and_convert_assets.sh
+++ scripts/assets/map_and_convert_assets.sh
@@ -14,6 +14,7 @@ tar -xzf "$(basename $WEBP_TOOLS_URL)"
 CWEBP_PATH="$WEBP_TOOLS_DIR/bin/cwebp"
 
 # Step 1: Build asset map from game data paths + sprites catalog
+echo "Converting game data sprites."
 ASSET_MAP=$(jq -r '.[]' "$GAMEDATA_PATHS_FILE" | while read -r NAME; do
     echo "$NAME" | jq -R --slurpfile sprites "$SPRITES_FILE" '
         . as $name |
@@ -40,13 +41,35 @@ echo "$ASSET_MAP" | jq -r 'to_entries[] | "\(.key)\t\(.value)"' | while IFS=$'\t
     fi
 done
 
-# Step 3: Copy other assets that don't need mapping and conversion
-# (so far just I18N)
+# Step 3: Copy other assets that don't come from game data mappings
+
+# Extra UI elements that aren't referenced by game data but are useful for building game-like UI elements
+declare -A EXTRA_PNG_CONVERSION_MAP=(
+    ["Assets/_Project/StaticAssets/_AddressedAssets/Sprites/Randy UI/Entities"]="UI/Frames"
+    ["Assets/_Project/StaticAssets/_AddressedAssets/Sprites/Randy UI/Badges"]="UI/Badges"
+)
+
+echo "Converting extra UI elements."
+for source_dir in "${!EXTRA_PNG_CONVERSION_MAP[@]}"; do
+    target_dir="${EXTRA_PNG_CONVERSION_MAP[$source_dir]}"
+    mkdir -p "publish/$target_dir"
+    echo "\"$EXTRACTED_DIR/${source_dir}\" -> \"publish/${target_dir}\""
+    for f in "$EXTRACTED_DIR/$source_dir"/*.png; do
+        [ -e "$f" ] || continue
+        name="$(basename "$f")"
+        "$CWEBP_PATH" -lossless "$f" -o "publish/$target_dir/${name}.webp"
+    done
+done
+
+# translation files
+echo "Copying translation files."
 mkdir -p "publish/I18N"
 for f in "$EXTRACTED_DIR"/Assets/_Project/StaticAssets/_AddressedAssets/I18N/*.bytes; do
     name="$(basename "$f")"
     cp -- "$f" "publish/I18N/${name%.bytes}.csv"
 done
 
+# font icon files
+echo "Copying icon fonts."
 mkdir -p "publish/fonts"
 cp "$EXTRACTED_DIR"/Assets/Resources/fonts/icons/*.otf publish/fonts/
